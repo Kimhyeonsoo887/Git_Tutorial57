81505b3f013913fadf80c18378ee24094ab9fc85
rlagustn8705 <rlagustn8705@naver.com>
2020-02-12 17:27:24 +0900

    Add userController [update]

--- userController.java
+++ userController.java
@@ -12,4 +12,10 @@ public Class UserController(){
 		logger.info("login: insert")
 		return "user/insert";
 	}
+
+	@RequestMapping("/user/update.do")
+	public String update(){
+		logger.info("update: update")
+		return "user/update";
+	}
 }
